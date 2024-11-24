1488702a55abe83350b80be377c8fd4878f69be9
EdyDevClub <redemasteroficiall@gmail.com>
2024-11-23 23:50:04 -0300

    Alteração Mobile

--- java.js
+++ java.js
@@ -1,12 +1,10 @@
 // Seleciona os botões pelos IDs
 const button1 = document.getElementById('btnCheckout1');
 const button2 = document.getElementById('btnCheckout2');
-
 // Adiciona eventos de clique para cada botão
 button1.addEventListener('click', function() {
     window.location.href = 'https://binolla.com/?lid=1626';
 });
-
 button2.addEventListener('click', function() {
     window.location.href = 'https://binolla.com/?lid=1626';
 });
